189af1126be43f19513ca279c53c427ed32a60af
Kevin Rivera <shimpo11@outlook.com>
2022-07-14 22:48:32 -0500

    -Creación de la pagina de Equipo -Agregando notificaciones form del index -Agregando imagenes de perfil Equipo -Modificando La Nav de la pagina Compras y Equipo

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
+//* Funcion para mostrar tooltip
 $(function () {
     $('[data-bs-toggle="tooltip"]').tooltip()
 })
+
+//* Funcion para mostrar una notificacion
+var option =
+{
+    animation: true,
+    delay: 2000
+};
+
+function Toasty() {
+    var toastHTMLElement = document.getElementById('liveToast');
+
+    var toastElement = new bootstrap.Toast(toastHTMLElement, option);
+
+    toastElement.show();
+}
